2a66a76d830b808d3334c1713f99d9b8d4a3fcd9
Renan Barbosa Da Silva <140984671+RenanBarbosa21@users.noreply.github.com>
2024-07-14 02:50:02 -0300

    primeiro commit

---
+++ treino01_C#/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treino01_C_
+{
+    internal class Calculator
+    {
+       //metodo publico para calcular de acordo os com os parametros listados.
+        public int Calculate(String op, int value1,int value2) {
+
+            if (op == "+") 
+            {
+                return value1 + value2;
+            }
+            if (op == "_") 
+            {
+                return value1 - value2 ;
+            }
+            if (op == "*") 
+            {
+                return value1 * value2;
+            }
+            if (op == "/") 
+            {
+                return value1 / value2;
+            }
+            return 0;
+        }
+    }
+}
---
+++ treino01_C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using treino01_C_;
+
+Console.WriteLine("digite o primeiro numero");
+var n1 = int.Parse (Console.ReadLine());
+Console.WriteLine("digite o segundo numero");
+var n2 = int.Parse (Console.ReadLine());
+Console.WriteLine("Coloque um operador");
+var operate = Console.ReadLine();
+
+Calculator calculator = new Calculator();
+
+Console.WriteLine(calculator.Calculate(operate, n1, n2));
+Console.ReadLine();
